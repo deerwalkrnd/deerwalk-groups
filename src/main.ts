d70319d8ab041db8002d1f4e3ba948d783f10a3a
Bijay Shrestha <bijayashrestha929@gmail.com>
2025-07-24 17:23:43 +0545

    updated

---
+++ src/main.ts
@@ -0,0 +1,253 @@
+import "./style.css";
+
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <!-- ====================================MODEL====================================  -->
+
+    <section class="model-wrapper invisible">
+      <div class="model-block animate__animated animate__zoomInUp">
+        <header class="model-header">
+          <div class="cross-icon-container">
+            <i class="bx bx-x"></i>
+          </div>
+        </header>
+        <div class="avatar-container">
+          <img
+            id="model_image"
+            src="../assets/images/avatar/first-person.png"
+            alt="picture of a person"
+          />
+        </div>
+
+        <h3 class="helvetica" id="model-name">Rudra Pandey</h3>
+        <h5 class="roboto-300" id="model-designation">
+          Chairperson Emeritus/Founder
+        </h5>
+
+        <p
+          class="roboto-400"
+          id="model-description"
+          style="text-align: justify"
+        >
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat
+          culpa odit magnam? Est quos expedita maiores commodi, alias ipsam
+          quis. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Repellat culpa odit magnam? Est quos expedita maiores commodi, alias
+          ipsam quis.
+        </p>
+      </div>
+    </section>
+
+    <!-- ====================================Screen Starts Here====================================  -->
+
+    <main id="home-screen">
+      <!-- ====================================hero section outer background====================================  -->
+      <section class="home-screen_container">
+        <!-- ====================================HERO SECTION INNER CONTAINER BLOCK====================================  -->
+
+        <div id="particles-js" class="large-block">
+          <!-- =================NAV -->
+          <nav>
+            <img
+              class="dwit-logo"
+              src="../assets/images/logo/dwit-logo-white.png"
+              alt="company logo"
+            />
+          </nav>
+
+          <!-- =================Testimonial Division (Section With Different Chief Head Testimonials) -->
+
+          <div class="testimonial-division">
+            <!--  top most upper container containing first & only one testimonial card  -->
+            <div class="upper-division">
+              <!-- our testimonial card starts here -->
+              <div class="testimonial animate__animated animate__fadeInUp">
+                <div class="avatar">
+                  <img
+                    class="testimonial-image"
+                    src="../assets/images/avatar/rudra_sir.png"
+                    alt="avatar"
+                  />
+                </div>
+                <p class="helvetica">Chairperson Emeritus / Founder</p>
+                <strong class="roboto-500">Rudra Pandey</strong>
+              </div>
+              <!-- our testimonial card ends here -->
+            </div>
+
+            <!--  middle container containing second & only one testimonial card  -->
+            <div class="middle-division">
+              <!-- our testimonial card starts here -->
+              <div class="testimonial animate__animated animate__fadeInUp">
+                <div class="avatar">
+                  <img
+                    class="testimonial-image"
+                    src="../assets/images/avatar/1.png"
+                    alt="avatar"
+                  />
+                </div>
+                <p class="helvetica">Chairperson</p>
+                <strong class="roboto-500">Hitesh Karki</strong>
+              </div>
+              <!-- our testimonial card ends here -->
+            </div>
+
+
+
+            <!--  lowest container containing second & only one testimonial card  -->
+            <div class="lower-division">
+              <!-- our testimonial card starts here -->
+
+              <div class="testimonial animate__animated animate__fadeInUp">
+                <div class="avatar">
+                  <img
+                    class="testimonial-image"
+                    src="../assets/images/avatar/pooja-neupane.png"
+                    alt="avatar"
+                  />
+                </div>
+                <p class="helvetica finance">Director of Finance</p>
+                <strong class="roboto-500">Pooja Neupane</strong>
+              </div>
+
+              <!-- Hariram Khadka -->
+              <div class="testimonial animate__animated animate__fadeInUp">
+                <div class="avatar">
+                  <img
+                    class="testimonial-image"
+                    src="../assets/images/avatar/hariram-khadka.png"
+                    alt="avatar"
+                  />
+                </div>
+                <p class="helvetica finance">Administration and Legal Officer</p>
+                <strong class="roboto-500">Hariram Khadka</strong>
+              </div>
+
+              <!-- our testimonial card starts here -->
+              
+              <!-- our testimonial card ends here -->
+
+              <!-- NEW TEAM MEMBERS START HERE -->
+
+              <div class="testimonial animate__animated animate__fadeInUp">
+                <div class="avatar">
+                  <img
+                    class="testimonial-image"
+                    src="../assets/images/avatar/nizu-dahal.png"
+                    alt="avatar"
+                  />
+                </div>
+                <p class="helvetica finance">Director of Administration</p>
+                <strong class="roboto-500">Nizu Dahal</strong>
+              </div>
+
+              <!-- Alisha Thapa Magar -->
+              <div class="testimonial animate__animated animate__fadeInUp">
+                <div class="avatar">
+                  <img
+                    class="testimonial-image"
+                    src="../assets/images/avatar/alisha-thapa-magar.png"
+                    alt="avatar"
+                  />
+                </div>
+                <p class="helvetica finance">Human Resources Officer</p>
+                <strong class="roboto-500">Alisha Thapa Magar</strong>
+              </div>
+              <!-- our testimonial card ends here -->
+
+              <!-- our testimonial card ends here -->
+            </div>
+          </div>
+
+          <!-- <div class="description-division">
+            <p class="roboto-300">
+              Deerwalk Institute of Technology is a private college established
+              in 2010 in collaboration between Nepalese entrepreneurs and the
+              United States-based software company, Deerwalk Inc. It is an
+              affiliation to Tribhuvan University, which is the oldest
+            </p>
+          </div> -->
+        </div>
+      </section>
+
+      <!-- 3 Card Container stats here  -->
+      <section class="featured-class_container">
+        <!--  featured card block starts here -->
+        <div class="featured-card-block">
+          <section>
+            <!-- 1st featured card starts here  -->
+            <a
+              class="text-decoration-none featured-cards"
+              href="https://deerwalk.edu.np/DWIT/"
+              target="_blank"
+            >
+              <div>
+                <img src="../assets/icons/dwit-no-text.png" alt="deerwalk icon" />
+              </div>
+              <h2 class="inter-700 featured-border--blue">
+                Deerwalk Institute of Technology
+              </h2>
+            </a>
+
+            <!-- 2nd featured card starts here  -->
+            <a
+              href="https://deerwalk.edu.np/sifalschool"
+              target="_blank"
+              class="text-decoration-none featured-cards"
+            >
+              <div>
+                <img src="../assets/icons/dwit-no-text.png" alt="deerwalk icon" />
+              </div>
+
+              <h2 class="inter-700 featured-border--orange">
+                Deerwalk Sifal School
+              </h2>
+            </a>
+
+            <!-- 3rd featured card starts here  -->
+            <a
+              class="text-decoration-none featured-cards"
+              href="https://deerwalk.edu.np/dhading-school"
+              target="_blank"
+            >
+              <div>
+                <img src="../assets/icons/dwit-no-text.png" alt="deerwalk icon" />
+              </div>
+              <h2 class="inter-700 featured-border--lightblue">
+                Deerwalk Dhading School
+              </h2>
+            </a>
+
+            <!-- 4th featured card starts here  -->
+            <a
+              href="https://deerwalk.edu.np/pokhara-school"
+              target="_blank"
+              class="text-decoration-none featured-cards"
+            >
+              <div>
+                <img src="../assets/icons/dwit-no-text.png" alt="deerwalk icon" />
+              </div>
+              <h2 class="inter-700 featured-border--jungle-green">
+                Deerwalk Pokhara School
+              </h2>
+            </a>
+
+            <!-- 5th featured card starts here  -->
+            <a
+              href="https://deerwalktrainingcenter.com/"
+              target="_blank"
+              class="text-decoration-none featured-cards"
+            >
+              <div>
+                <img src="../assets/icons/dwit-no-text.png" alt="deerwalk icon" />
+              </div>
+              <h2 class="inter-700 featured-border--cyan">Deerwalk Compware</h2>
+            </a>
+          </section>
+        </div>
+      </section>
+    </main>
+
+    <!-- =====================================footer starts here  -->
+    <footer class="primary-footer inter-700">Sifal, Kathmandu, Nepal</footer>
+`;
---
+++ src/script.ts
@@ -0,0 +1,117 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const button = document.getElementsByClassName("testimonial");
+    const model = document.getElementsByClassName("model-wrapper")[0];
+    const close_model_icon = document.getElementsByClassName(
+        "cross-icon-container",
+    )[0];
+
+    for (let i = 0; i < button.length; i++) {
+        button[i].addEventListener("click", function () {
+            // this.classList.toggle("active");
+            model.classList.toggle("invisible");
+        });
+
+        close_model_icon.addEventListener("click", function () {
+            model.classList.add("invisible");
+        });
+    }
+
+    // ========================Dynamic Logic for testimonial model===========================
+
+    const testimonialDataSet = [
+        {
+            name: "Rudra Pandey",
+            designation: "Chairperson Emeritus / Founder",
+            description:
+                "Rudra is a pioneer in the software outsourcing industry in Nepal with significant IT experience in the financial service industry. He received his Bachelor's degree in Electrical Engineering from Pakistan and PhD in Economics from Northeastern University, USA. His vision is to create a comprehensive learning in Nepal focusing on technology and its application for the benefit of the country.",
+        },
+        {
+            name: "Hitesh Karki",
+            designation: "Chairperson",
+            description:
+                "Hitesh's core responsibility is to look after the curriculum, ensure timely execution of classes and help connect the students with the happenings of the industry. He completed his Bachelor's degree in Computer Science, from Ferguson College, Pune University, his MBA (Executive) from Kathmandu University (KU) and is currently pursuing his PhD from Kathmandu University.",
+        },
+        // {
+        //     name: "Samjhana Pokhrel",
+        //     designation: "Vice President of Academic Affairs",
+        //     description:
+        //         "Samjhana brings over eight years of experience in higher education, having contributed to key areas such as Admissions, Administration, Examination, and Placement. As Vice President of Academic Affairs, she is dedicated to advancing academic quality, driving innovation, and strengthening institutional growth.She holds a Bachelor's in Information Management and a Master's in English Literature from Pokhara University, along with a Master's in Data Science from the University of Westminster, London—reflecting her interdisciplinary expertise across humanities, management, and technology.With a strong blend of academic and administrative insight, Samjhana is committed to fostering a forward-thinking, student-focused academic environment.",
+        // },
+        {   
+            name: "Pooja Neupane",
+            designation: "Director of Finance",
+            description:
+                "Pooja Neupane has recently assumed the role of Associate Director of Finance at Deerwalk Group Limited. Holding a Chartered Accountant designation from the Institute of Chartered Accountants of India, her professional journey includes notable experience at TR Upadhya and Co., where she honed her skills in accounting and finance.",
+        },
+        // {
+        //     name: "Ujjwal Poudel",
+        //     designation: "Vice Principal / DSS Administrative Affairs",
+        //     description:
+        //         "Ujjwal holds a Master's Degree in Physics from Tribhuvan University. He has more than 12 years of teaching experience. He is an alumnus of the European Council for Nuclear Research (CERN) Teachers Programme 2022 and an official CERN ambassador for promoting Science and Technology in Nepal. Since joining Deerwalk in 2016, he has worked in various roles and led educational projects preparing online learning content for Nepal. Currently, he works as the Vice Principal of Deerwalk Sifal School.",
+        // },
+        
+        // {
+        //     name: "Madhu Sudhan Bhusal",
+        //     designation: "Vice Principal / DSS Academic Affairs",
+        //     description:
+        //         "Mr. Bhusal holds Master of Science in Physics from Tribhuvan University. He is the Head of High School & He teaches Physics.",
+        // },
+        // {
+        //     name: "Nirmal Kumar Paudel",
+        //     designation: "DDS Principal",
+        //     description:
+        //         "Nirmal Kumar Paudel is currently serving as the Principal of Deerwalk Dhading School. With a Master's Degree in Education (M. Ed.), he brings 17 years of extensive teaching experience. Throughout his career, he has held various roles at Deerwalk Sifal School, including Head of High School, Program Officer, and Vice Principal of Administrative Affairs. He has completed eight years of dedicated service at Deerwalk and is now in his ninth year.",
+        // },
+        // NEW TEAM MEMBERS DATA
+        
+        {
+            name: "Hariram Khadka",
+            designation: "Administration and Legal Officer",
+            description:
+                "Hariram Khadka is the Admin Officer at Deerwalk Group, responsible for overseeing administrative operations and ensuring smooth day-to-day functioning of the organization. His attention to detail and organizational skills make him an invaluable asset to the administrative team, supporting various departments in their operational needs.",
+        },
+        {
+            name: "Nizu Dahal",
+            designation: "Director of Administration - DWIT",
+            description:
+                "Nizu Dahal brings over 14 years of professional experience in people and culture management, spanning roles in hospitals, software companies, and academia. With a seamless transition into higher education, Nizu is currently dedicated to nurturing future leaders and contributing to the institutional growth of Deerwalk Institute of Technology. Her academic background includes a Master's degree in International Relations and Diplomacy from Tribhuvan University, which equips her to foster positive student relations, prepare students for future employment, and enhance organizational dynamics.",
+        },
+        {
+            name: "Alisha Thapa Magar",
+            designation: "Human Resource Officer",
+            description:
+                "Alisha Thapa Magar serves as the Associate Human Resource Officer at Deerwalk Group. With her expertise in human resources management, she plays a crucial role in talent acquisition, employee development, and organizational culture enhancement. Her dedication to fostering a positive work environment contributes significantly to the company's growth and employee satisfaction.",
+        },
+    ];
+
+    const testimonialNode = document.getElementsByClassName("testimonial");
+
+    // put add event listener on each testimonial
+    for (let i = 0; i < testimonialNode.length; i++) {
+        testimonialNode[i].addEventListener("click", function () {
+            const testimonial_image =
+                document.getElementsByClassName("testimonial-image")[i];
+            const testimonial_image_url = testimonial_image.getAttribute("src") as string;
+            // console.log(testimonial_image_url);
+            const model_img_node = document.getElementById("model_image");
+            if (model_img_node == null) {
+                console.log("model_img_node is null");
+                return;
+            }
+            model_img_node.setAttribute("src", testimonial_image_url);
+
+            const model_name = document.getElementById("model-name");
+            const model_designation = document.getElementById("model-designation");
+            const model_description = document.getElementById("model-description");
+
+            if (model_name == null || model_designation == null || model_description == null) {
+                console.log("model_name, model_designation, model_description is null");
+                return;
+            }
+            model_name.innerHTML = testimonialDataSet[i].name;
+            model_designation.innerHTML = testimonialDataSet[i].designation;
+            model_description.innerHTML = testimonialDataSet[i].description;
+        });
+    }
+})
